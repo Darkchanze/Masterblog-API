729dda4185644c8d6c981419154fa206880aaf6c
Darkchanze <nico.wittemann@t-online.de>
2024-12-11 09:08:35 +0100

    Sorted in get_posts implemented

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,5 +1,3 @@
-from re import search
-
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
@@ -14,6 +12,12 @@ POSTS = [
 
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
+    """Handle GET and POST requests for posts.
+    - GET: Get all posts. It is optional to sort them by sending query strings.
+           Posts will be sorted by title or content when giving query string sort.
+           Posts will be sorted by in ascending or descending when giving query string direction.
+    - POST: Add a new post with title and content.
+    """
     if request.method == 'POST':
         frontend_data = request.get_json()
         if frontend_data:
@@ -32,11 +36,34 @@ def get_posts():
             return jsonify(new_post), 201
         else:
             return jsonify({"error": "Missing JSON"}), 400
-    return jsonify(POSTS), 200
+    if request.method == 'GET':
+        direction = request.args.get('direction')
+        sort = request.args.get('sort')
+        if sort or direction:
+            if sort is None:
+                sort = 'title'
+            elif sort != "title" and sort != "content":
+                return jsonify({"error": f"False input", "info": "sort has to be 'title' or 'content' if given."}), 400
+            if direction is None:
+                direction = True
+            elif direction == 'asc':
+                direction = False
+            elif direction == 'desc':
+                direction = True
+            else:
+                return jsonify({"error": f"False input", "info":  "direction has to be 'asc' or 'desc' if given."}), 400
+            sorted_posts = sorted(POSTS, key=lambda post: post[sort], reverse=direction)
+            return jsonify(sorted_posts), 200
+        return jsonify(POSTS), 200
 
 
 @app.route('/api/posts/<id>', methods=['DELETE', 'PUT'])
 def delete_post(id: str):
+    """Handle DELETE and PUT requests for a specific post by ID.
+
+    - DELETE: Remove a post by its ID.
+    - PUT: Update a post's title or content by its ID.
+    """
     for post in POSTS:
         if post['id'] == int(id):
             if request.method == 'DELETE':
@@ -52,7 +79,8 @@ def delete_post(id: str):
 
 
 @app.route('/api/posts/search', methods=['GET'])
-def search_for_post():   # you get query parameters!!!
+def search_for_post():
+    """Search for posts by title or content keywords."""
     search_title = request.args.get('title')
     search_content = request.args.get('content')
     filtered_posts = []
@@ -73,13 +101,10 @@ def search_for_post():   # you get query parameters!!!
     return jsonify(filtered_posts), 200
 
 
-
-
-
-
-
-
-
-
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
+
+
+
+#todo 2. Upload Read page Conclusion and Next Steps
+#todo 3. Upload to Codio
